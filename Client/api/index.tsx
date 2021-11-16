4327b1492b0d153ff47d131066c3f2c509176ffd
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-11-16 16:44:22 +0000

    adding back end routes

--- Client/api/index.tsx
+++ Client/api/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API = axios.create({
 // 	baseURL: 'http://localhost:5001',
-// });
+// })
 
 const API = axios.create({
 	baseURL: 'https://poke-decks-uk.herokuapp.com',
