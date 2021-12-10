ac9577ca0411d78012344a9870d81217b4dfb897
Jamie Tardi <jamie.tardi@gmail.com>
2021-12-10 08:07:16 +0000

    sync with main pc

--- Server/controllers/orders.js
+++ Server/controllers/orders.js
@@ -4,8 +4,17 @@ export const addOrder = async (req, res) => {
 	console.log(req);
 
 	const allOrders = await orders.find();
+	const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+	let orderLetters = [];
 
-	const orderNumber = 'poke-order-' + '' + '000' + allOrders.length.toString();
+	for (let i = 0; i < 4; i++) {
+		let num = Math.floor(Math.random() * 10);
+		orderLetters.push(letters[num]);
+		console.log(num);
+	}
+	console.log(orderLetters);
+	const orderNumber =
+		'poke-' + orderLetters.join('') + '-' + '000' + allOrders.length.toString();
 
 	console.log(orderNumber);
 	//continue here tommoro
