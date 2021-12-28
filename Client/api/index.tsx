dd450706d8a7768481b661477953d30e5f087666
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-12-28 18:45:57 +0000

    removing useless api end points

--- Client/api/index.tsx
+++ Client/api/index.tsx
@@ -78,66 +78,10 @@ export const updateUser = (
 
 //Products
 
-export const createProduct = (
-	formData: object,
-	setIsLoading: any,
-	setInfoText: any,
-) => {
-	API.post('/products', formData, setInfoText)
-		.then(() => setIsLoading(false))
-		.then(() => setInfoText('Product added you awesome person! ✨'))
-		.catch((err) => setInfoText(err.response.data.message));
-};
-
-export const addExpansions = (
-	expansion: object,
-	setIsLoading: any,
-	setInfoText: any,
-) => {
-	API.post('/expansion', expansion)
-		.then(() => {
-			setInfoText('Expansion added you awesome person! ✨');
-		})
-		.then(() => setIsLoading(false))
-
-		.catch((err) => setInfoText(err.response.data.message));
-};
-
-export const addCategories = (
-	addCategory: object,
-	setIsLoading: any,
-	setInfoText: any,
-) => {
-	API.post('/category', addCategory)
-		.then(() => {
-			setInfoText('Category added you awesome person! ✨');
-		})
-		.then(() => setIsLoading(false))
-		.catch((err) => setInfoText(err.response.data.message));
-};
-
 export const getCategories = (setCategories: any) => {
 	API.get('/category')
 		.then((res) => setCategories(res.data.categories))
 		.catch((err) => console.log(err.response.data.message));
-};
-
-export const updateCategories = (
-	id: string,
-	updateCategory: object,
-	setInfoTxt: any,
-) => {
-	API.patch(`/category/${id}`, updateCategory)
-		.then((res) => setInfoTxt('Updated item'))
-		.catch((err) =>
-			setInfoTxt('Something Went wrong, refresh the page and try again.'),
-		);
-};
-
-export const updateExpansion = (id: string, updateExpansion: object) => {
-	API.patch(`/expansion/${id}`, updateExpansion)
-		.then((res) => console.log(res))
-		.catch((err) => console.log(err));
 };
 
 export const getExpansions = (setExpansions: any) => {
@@ -146,28 +90,9 @@ export const getExpansions = (setExpansions: any) => {
 		.catch((err) => console.log(err.response.data.message));
 };
 
-export const deleteExpansion = (id: string, setInfoText: Function) => {
-	API.delete(`/expansion/${id}`)
-		.then((res) => setInfoText(res.data))
-		.catch(() => setInfoText('Something went wrong'));
-};
-
-export const deleteCategory = (id: string, setInfoText: Function) => {
-	API.delete(`/category/${id}`)
-		.then((res) => setInfoText(res.data))
-		.catch(() => setInfoText('Something went wrong'));
-};
-
 export const getAllProducts = (setProducts: any) => {
 	API.get('/products')
 		.then((res) => setProducts(res.data))
-		.catch((err) => console.log(err));
-};
-
-export const deleteProduct = (id: any, setDeleteResponse: any) => {
-	API.delete(`/products/${id}`)
-
-		.then(() => setDeleteResponse(true))
 		.catch((err) => console.log(err));
 };
 
@@ -180,18 +105,6 @@ export const getProductByCat = (cat: string, setProducts: Function) => {
 export const getProductByExp = (exp: string, setProducts: Function) => {
 	API.get(`/products/expansions/${exp}`)
 		.then((res) => setProducts(res.data))
-		.catch((err) => console.log(err));
-};
-
-//updating
-
-export const updateProduct = (
-	id: string,
-	formData: object,
-	setIsLoading: Function,
-) => {
-	API.patch(`/products/${id}`, formData)
-		.then(() => setIsLoading(false))
 		.catch((err) => console.log(err));
 };
 
@@ -284,36 +197,6 @@ export const createOrder = (
 		.catch((err) => console.log(err));
 };
 
-export const deleteOrder = (id: string, setDeleteMessage: Function) => {
-	API.delete(`/orders/delete/${id}`)
-		.then(() => setDeleteMessage('Item deleted from database! 🔥'))
-		.catch((err) => setDeleteMessage(err.message));
-};
-
-export const getAllOrders = (setOrders: Function, setIsLoading: Function) => {
-	API.get('/orders')
-		.then((res) => setOrders(res.data))
-		.then(() => setIsLoading(false))
-		.catch((err) => console.log(err));
-};
-
-export const getTotals = (setMonthTotal: Function, setIsLoading: Function) => {
-	API.get('/orders/totals')
-		.then((res) => setMonthTotal(res.data))
-		.then(() => setIsLoading(false))
-		.catch((err) => console.log(err));
-};
-
-export const getWeeklyTotals = (
-	setWeeklyTotals: Function,
-	setIsLoading: Function,
-) => {
-	API.get('/orders/weekly')
-		.then((res) => setWeeklyTotals(res.data))
-		.then(() => setIsLoading(false))
-		.catch((err) => console.log(err));
-};
-
 export const updateShipping = (
 	id: string,
 	customer: object,
@@ -327,33 +210,10 @@ export const updateShipping = (
 
 //Promtional offers
 
-export const removePromo = (id: string) => {
-	API.delete(`/promotions/${id}`)
-		.then((res) => console.log(res.data))
-		.catch((err) => console.log(err.response));
-};
-
-export const createPromotion = (promotion: object) => {
-	API.post('/promotions', promotion)
-		.then((res) => console.log(res.data))
-		.catch((err) => console.log(err.response));
-};
-
 export const getPromotions = (setPromotions: Function) => {
 	API.get('/promotions')
 		.then((res) => setPromotions(res.data))
 		.catch((err) => console.log(err));
-};
-
-export const updatePromotion = (
-	id: string,
-	promotion: object,
-	setIsLoading: Function,
-) => {
-	API.patch(`/promotions/${id}`, promotion)
-		.then((res) => console.log(res.data))
-		.then(() => setIsLoading(false))
-		.catch((err) => console.log(err.message));
 };
 
 export const applyPromoCode = (
