4ba3e9e739de72117349803c723034e25d1a1989
Jamie tARDI <JAMIE.TARDI@GMAIL.COM>
2021-12-31 10:46:25 +0000

    test
    -Item 1
    -Item 2

--- Client/pages/password/reset.tsx
+++ Client/pages/password/reset.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface Props {}
 
 const PasswordReset = (props: Props) => {
-	return <div>Password reset</div>;
+	return <div>Password resets</div>;
 };
 
 export default PasswordReset;
