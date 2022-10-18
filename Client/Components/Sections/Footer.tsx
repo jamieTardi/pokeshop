949aa941e0263cea980d8e33b84d8695488e801c
Jamie Tardi <jamie@razorsecure.com>
2022-10-18 09:58:14 +0100

    address update

--- Client/Components/Sections/Footer.tsx
+++ Client/Components/Sections/Footer.tsx
@@ -1,54 +1,47 @@
-import { Avatar } from '@mui/material';
-import {
-	FacebookOutlined,
-	YouTube,
-	Twitter,
-	Instagram,
-} from '@mui/icons-material';
-import styles from '../../styles/Home.module.scss';
-import visa from '../../Images/Visa.png';
-import MasterCard from '../../Images/MasterCard.png';
-import AmericanExpress from '../../Images/AmericanExpress.png';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Avatar } from "@mui/material";
+import { FacebookOutlined, YouTube, Twitter, Instagram } from "@mui/icons-material";
+import styles from "../../styles/Home.module.scss";
+import visa from "../../Images/Visa.png";
+import MasterCard from "../../Images/MasterCard.png";
+import AmericanExpress from "../../Images/AmericanExpress.png";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {}
 
 const date = new Date();
 
 const logos = [
-	<a href='https://www.facebook.com/poke.decks.uk' className={styles.footerSVG}>
+	<a href="https://www.facebook.com/poke.decks.uk" className={styles.footerSVG}>
 		<FacebookOutlined key={1} />
 	</a>,
 	// <a href='' className={styles.footerSVG}>
 	// 	<YouTube key={2} />
 	// </a>,
-	<a href='https://twitter.com/pokedecks_uk' className={styles.footerSVG}>
+	<a href="https://twitter.com/pokedecks_uk" className={styles.footerSVG}>
 		<Twitter key={3} />
 	</a>,
-	<a
-		href='https://www.instagram.com/poke.decks.uk/'
-		className={styles.footerSVG}>
+	<a href="https://www.instagram.com/poke.decks.uk/" className={styles.footerSVG}>
 		<Instagram key={4} />
 	</a>,
 ];
 
 const payments: Array<any> = [
-	<Image src={visa} alt='card' width={50} height={20} />,
-	<Image src={MasterCard} alt='card' width={50} height={30} />,
-	<Image src={AmericanExpress} alt='card' width={50} height={30} />,
+	<Image src={visa} alt="card" width={50} height={20} />,
+	<Image src={MasterCard} alt="card" width={50} height={30} />,
+	<Image src={AmericanExpress} alt="card" width={50} height={30} />,
 ];
 
 const Footer = (props: Props) => {
 	return (
-		<div style={{ position: 'relative', width: '90%' }}>
+		<div style={{ position: "relative", width: "90%" }}>
 			<div className={styles.footerGrid}>
 				<div className={styles.footerGridItem1}>
 					<h4>Our Store</h4>
 					<p>GirlCode trading as Poke-Decks</p>
 					<p>
-						55 fountain road,
-						<br /> Harborne, <br /> Birmingham, <br /> B178NP
+						36 Gathorne Road,
+						<br /> Bristol, <br /> BS31LU
 					</p>
 					<p>&#169;{date.getFullYear()}</p>
 				</div>
@@ -57,15 +50,12 @@ const Footer = (props: Props) => {
 					<h5>Help and Information</h5>
 					<ul>
 						<li>
-							<a
-								href='/Cookie-Policy-Poke-Decks.pdf'
-								rel='noopener noreferrer'
-								download>
+							<a href="/Cookie-Policy-Poke-Decks.pdf" rel="noopener noreferrer" download>
 								Cookie Policy
 							</a>
 						</li>
 						<li>
-							<a href='/Privacy_policy.pdf' rel='noopener noreferrer' download>
+							<a href="/Privacy_policy.pdf" rel="noopener noreferrer" download>
 								Privacy Policy
 							</a>
 						</li>
@@ -75,16 +65,13 @@ const Footer = (props: Props) => {
 					<h5>Your Order</h5>
 					<ul>
 						<li>
-							<a
-								href='/Terms_and_Conditions.pdf'
-								rel='noopener noreferrer'
-								download>
+							<a href="/Terms_and_Conditions.pdf" rel="noopener noreferrer" download>
 								Terms and Conditions
 							</a>
 						</li>
 
 						<li>
-							<a href='/Returns_policy.pdf' rel='noopener noreferrer' download>
+							<a href="/Returns_policy.pdf" rel="noopener noreferrer" download>
 								Returns Policy
 							</a>
 						</li>
@@ -94,7 +81,7 @@ const Footer = (props: Props) => {
 					<h5>Follow Us</h5>
 					<div className={styles.footerGridItems}>
 						{logos.map((logo, i) => (
-							<Avatar key={i} sx={{ bgcolor: '#E84875', margin: '0% 2%' }}>
+							<Avatar key={i} sx={{ bgcolor: "#E84875", margin: "0% 2%" }}>
 								{logo}
 							</Avatar>
 						))}
